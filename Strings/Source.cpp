1c4189cffb83306c46ddc555f84364a019270087
Ж - 16 < - 16@DESKTOP-MKQBEK5>
2022-03-18 20:35:24 +0500

    Add project files.

---
+++ Strings/Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	//Задача 1
+
+	
+
+
+
+
+
+
+/*char sym = 'n';
+	sym = toupper(sym); // функция toupper
+	cout << sym << endl;
+	sym = tolower(sym); // функция tolower
+	cout << sym << endl;
+
+	string str = "Hello";
+	for (int i = 0; i < str.size(); i++)
+		str[i] = tolower(str[i]);
+	cout << str << endl;*/
+/*string str;
+	cout << "Введите строку:\n";
+	getline(cin, str);
+	int n = stoi(str); //функция stoi
+	n++;
+	cout << n << endl;
+	string str2 = to_string(n) + "abc"; // функция to_string
+	cout << str2 << endl;*/
+    //cout << "Длина строки = " << str.length() << endl;
+	//cout << "Длина строки = " << str.size() << endl;
+	//string str2 = "123";
+	//str.insert(2, str2); //Метод insert
+	//str.replace(2,3,str2); //Метод replace
+	//cout << str << endl;
+	//cout << "Введите подстроку:\n";
+	//getline(cin, str2);
+	//cout << str.find(str2) << endl; // Метод find
+	//cout << str.rfind(str2) << endl; // Метод rfind
+//Escape последовательности в строках
+//cout << str.substr(6, 3) << endl; // Метод substr
+	/*string str = "1234\n5678";
+	cout << str;*/
+
+
+//Ввод строки через консоль
+	/*cout << "Do you want to play? (Yes/No) -> ";
+	string answer;
+	getline(cin,answer);
+
+
+	string name;
+	cout << "What's your name? -> ";
+	getline(cin, name);
+	cout << "Hi, " << name << "!" << endl;
+	cout << "How old are you? -> ";
+	int age;
+	cin >> age;
+	cout << age << endl;*/
+
+	//Символьные массивы
+	/*char word[] = "Hello!";
+	for (int i = 0; i < 6; i++)
+		cout << word[i];
+	cout << endl;
+	cout << word << endl;
+	cout << word[5] << word[0] << endl;
+	char sym = word[3];
+	cout << sym << endl;*/
+
+	//Строковые переменные
+	/*string str = "1 + 1 = 2";
+	cout << str << endl;
+	cout << str[0] << str[4] << endl;
+	string str2 = str + "!!";
+	cout << str2 << endl;
+	str += "000";
+	cout << str << endl;*/
+
+	return 0;
+}
